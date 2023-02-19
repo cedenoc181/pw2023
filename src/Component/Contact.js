0688f758caa0524d84ba918d240da50a897507a6
Christian <cedenoc181@gmail.com>
2023-02-19 17:40:07 -0500

    adding contact information into contact js

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className=" w-100 vh-100 bg-dark d-flex flex-column justify-content-center align-items-center">
+        <p><span>04. </span>Thank you for visiting!</p>
+        <h1>Feel free to reach out to me</h1>
+        <p>I am actively seeking new opportunites in Software engineering positions where i can apply 
+          and expand myu skills in programming, problem-solving, and teamwork. I am confiodent in my 
+          ability to contribute to any Software development project and work collaboratively with 
+          cross-functional teams. I am excited about the prospect of exploring new roles and finding 
+          a position that aligns with my career goals and values.
+        </p>
+        <button className='bng-transparent'>Say Hello!</button>
+    </div>
   )
 }
 
