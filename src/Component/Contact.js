1cb540f357b8d071ec32f93c484bcc449a27f6fd
Christian <cedenoc181@gmail.com>
2024-01-19 05:32:27 -0500

    made section 4 centered with the conditionn rendering. added new class that allowed me to center the css

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -39,7 +39,7 @@ function sendEmail (e) {
   return (
   <div id='contact' className="contact w-100 vh-100 bg-dark d-flex flex-column justify-content-center align-items-center">
     <div className='main-forum w-50 vh-100 text-center' data-aos ="flip-right" data-aos-delay="500" data-aos-duration="1500">
-      {emailMe ? ( <div>
+      {emailMe ? ( <div className="email-prompt">
         <p className='tyv'><span className='span'>04. </span>Thank you for visiting!</p>
         <h1 className='text-white'>Feel free to reach out to me</h1><p className='detail'>I am actively seeking new opportunites in Software engineering positions where i can apply 
           and expand my skills in programming, problem-solving, and teamwork. I am confident in my 
@@ -47,18 +47,11 @@ function sendEmail (e) {
           cross-functional teams. I am excited about the prospect of exploring new roles and finding 
           a position that aligns with my career goals and values.
         </p></div> ) : (
-          <div> 
+          <div className='form-container'> 
             <form ref={form} onSubmit={sendEmail}className=" form text-white justify-content-center text-align-center mt-5 ">
               <h3 className="form-prompt">Expect a response within 24 hours! </h3>
-              <br />
               <input className="bg-transparent item text-white" type="name" placeholder="Name here..." name="name"/>
-              <br />
-              {/* <label>Email: </label> */}
-              <br />
               <input className="bg-transparent item text-white" type="email" placeholder='Email here...' name="email"/>
-              <br />
-              {/* <label className="message">Message: </label> */}
-              <br />
               <textarea className="bg-transparent text-white" rows="5" id="text-box" placeholder='message here...' name="message"/>
               <br />
               <input className="bg-transparent text-white button " type="submit" value="Send"/>
