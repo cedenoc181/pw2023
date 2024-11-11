2ee941c7c5e6100f6d355948391d7b731ac9c4a6
Christian <cedenoc181@gmail.com>
2024-11-11 08:19:07 -0500

    changed the project from PT connect and added majesticpips, also made title links web accessible

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -37,21 +37,23 @@ function handleClick3() {
         <div class="row row-cols-1 row-cols-md-3 g-5 p-5 text-center">
           <div class="col" data-aos = 'fade-right' data-aos-delay="500" data-aos-duration="1500">
             <div className="card text-white">
-              <img src={require("../Images/PTconnect.jpg")} class="card-img-top" alt="PTconnect" />
+              <img src={require("../Images/Majesticpips.png")} class="card-img-top" alt="PTconnect" />
               <div className="card-body bg-dark">
-                <h5 class="card-title text-decoration-underline">PTconnect</h5>
+                <a href="https://majesticpips.com/">
+                <h5 class="card-title text-decoration-underline text-white">MajesticPips.com</h5>
+                </a>
                 <p class="card-text ">
-                  A booking application for Physical therapist looking to grow their clientel in NYC. 
-                  PTconnect holds a database of providers all over nyc. Providing users 
-                  access to top professionals that meet their needs.  
-                    Website luanch soon.
+                    Majesticpips.com, a currency trading community built using MERN stack. 
+                    This application is the landing page for Majesticpips Telegram/discord currently hosting a community of hundreds of members.
                 </p>
                 <button className='bg-transparent' onClick={handleClick}>{git ? "Load More!" : "Close"}</button>
                 {git ? (null) : (
                 <div className="link-con">
                   <a className="project-link" href="https://github.com/cedenoc181/pt-connect-backend-"><span>1. </span>Backend Repository</a>
                   <br />
-                  <a className="project-link" href="https://github.com/cedenoc181/Pt-connect-frontend-"><span>2. </span>Frontend Repository</a>
+                  <a className="project-link" href="https://github.com/cedenoc181/Majesticpips-client.git"><span>2. </span>Frontend Repository</a>
+                  <br />
+                  <a className="project-link" href="https://majesticpips.com/"><span>3. </span> Website</a>
                 </div>
                 )}
               </div>
@@ -61,7 +63,9 @@ function handleClick3() {
             <div className="card text-white">
               <img src={require("../Images/prohealth-logo.png")} class="card-img-top pb-3" alt="Project 2" />
               <div className="card-body bg-dark">
-                <h5 class="card-title text-decoration-underline">Pro Health and Fitness QR code</h5>
+                <a href="https://prohealthptot.info/">
+                <h5 class="card-title text-decoration-underline text-white">ProHealthPTOT.info</h5>
+                </a>
                 <p class="card-text">
                   This is JavaScript application I built and deployed for Pro Health and Fitness physical therapy.
                    The intentions was to use Qr codes to improve the accessibility for patients to leave a review about their experience.
@@ -72,7 +76,7 @@ function handleClick3() {
                 <div className="link-con">
                   <a className="project-link" href="https://github.com/cedenoc181/Prohealth-qrCode.git"><span>1. </span>Repository</a>
                   <br />
-                  <a className="project-link" href="https://prohealthptot.netlify.app/"><span>2. </span>Website</a>
+                  <a className="project-link" href="https://prohealthptot.info/"><span>2. </span>Website</a>
                 </div>
                 )}
               </div>
@@ -82,15 +86,17 @@ function handleClick3() {
             <div className="card text-white">
               <img src={require("../Images/moviehub-cover.png")} class="card-img-top pb-0" alt="Project 3" />
               <div className="card-body bg-dark">
-                <h5 class="card-title text-decoration-underline">LeMovie hub App</h5>
+              <a href="https://lemoviehub.netlify.app/">
+                <h5 class="card-title text-decoration-underline text-white">LeMovieHub.App</h5>
+              </a>
                 <p class="card-text">
-                React js application for movie query. Have a movie in mind? type it in and API will spit something out!
-                 This application use React, google fonts and OMDB API. Optomized for mobile devices.
+                React js application for movie query. Have a movie in mind? type it in and API will spit something out! 
+                This application use React, google fonts and OMDB API. Optomized for mobile devices.
                 </p>
                 <button className='bg-transparent' onClick={handleClick3}>{git3 ? "Load More!" : "Close"}</button>
                 {git3 ? (null) : (
                 <div className="link-con">
-                  <a className="project-link" href="https://github.com/cedenoc181/LeMovieHub.git"><span>1. </span>Frontend Repository</a>
+                  <a className="project-link" href="https://github.com/cedenoc181/LeMovieHub.git"><span>1. </span>Repository</a>
                   <br />
                   <a className="project-link" href="https://lemoviehub.netlify.app/"><span>2. </span>website</a>
                 </div>
